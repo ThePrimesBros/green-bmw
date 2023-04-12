8eb11e0de4650d6e7016135d51de85eda88d4c01
ThePrimesBros <thesket5@outlook.fr>
2023-04-12 14:15:10 +0200

    Commit

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,40 +1,55 @@
-import CardCard from '@/components/CarCard'
-import React from 'react'
-import { useGetCars } from '@/hooks'
+import CardCard from "@/components/CarCard";
+import React from "react";
+import { useGetCars } from "@/hooks";
 
 export default function ProductsPage() {
-    const [pageNumber, setPageNumber] = React.useState(1)
+  const [pageNumber, setPageNumber] = React.useState(1);
 
-    const { cars, isFetching } = useGetCars(pageNumber, 6)
-    const [allCars, setAllCars] = React.useState([])
+  const { cars, isFetching } = useGetCars(pageNumber, 6);
+  const [allCars, setAllCars] = React.useState([]);
 
-    const keepCars = React.useCallback(() => {
-        setAllCars([...allCars, ...cars])
-    }, [allCars, cars])
+  const keepCars = React.useCallback(() => {
+    setAllCars([...allCars, ...cars]);
+  }, [allCars, cars]);
 
-    const handleLoadMore = React.useCallback(() => {
-        setPageNumber(pageNumber + 1)
-        keepCars()
-    }, [keepCars, pageNumber])
+  const handleLoadMore = React.useCallback(() => {
+    setPageNumber(pageNumber + 1);
+    keepCars();
+  }, [keepCars, pageNumber]);
 
-    React.useEffect(() => {
-        setAllCars(cars)
-    }, [cars])
+  React.useEffect(() => {
+    setAllCars(cars);
+  }, [cars]);
 
-    return (
-        <React.Fragment>
-            <div className='h-full w-full py-2 overflow-auto md:px-32 relative space-y-10'>
-                <div className='grid grid-cols-1 gap-4 grid-row-2 grid-flow-dense md:grid-cols-3'>
-                    {!isFetching && !!allCars ? allCars?.map((car) => {
-                        return <CardCard key={car.id} {...car} />
-                    }) : <span>Loading...</span>}
-                </div>
-                <div className='w-full flex justify-center items-center'>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={handleLoadMore}>
-                        Load More
-                    </button>
-                </div>
-            </div>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <div className="h-full w-full py-2 overflow-auto md:px-32 relative space-y-10">
+        <div className="grid grid-cols-1 gap-4 grid-row-2 grid-flow-dense md:grid-cols-3">
+          {!isFetching && !!allCars ? (
+            allCars?.map((car, index) => {
+              return (
+                <CardCard
+                  key={car.id}
+                  name={car.identity.name}
+                  src={car.identity.mainPicture.src}
+                  id={car.id}
+                  index={index}
+                />
+              );
+            })
+          ) : (
+            <span>Loading...</span>
+          )}
+        </div>
+        <div className="w-full flex justify-center items-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleLoadMore}
+          >
+            Load More
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
